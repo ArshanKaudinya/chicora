9b1a4311f4db2702d13b91cf15d15bf3df43d217
Arshan Kaudinya <86201452+ArshanKaudinya@users.noreply.github.com>
2025-02-19 19:37:33 +0530

    routerfixig

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Explore from './pages/explore';
 
 function App() {
   return (
-    <Router>
-      <div className="bg-white min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter basename="/chicora">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
